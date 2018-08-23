d145fccf9fa1c2c5b36ffe70d4d9e608c284ebe9
Rajan Jangra <rajangra@users.noreply.github.com>
2018-08-23 19:38:50 -0400

    portfolio files added
    added the rest of the files

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(function () {
+  count = 0;
+  wordsArray = [" who cares."," focused on solving real problems."," who gets sh*t done."];
+  setInterval(function () {
+    count++;
+    $("#word").fadeOut(400, function () {
+      $(this).text(wordsArray[count % wordsArray.length]).fadeIn(400);
+    });
+  }, 5000);
+});
+
+$('#work, #work1').click(function(){
+    $("html, body").animate({ scrollTop: 400 }, 500);
+    return false;
+ });
+
+ $(document).ready(function(){
+  //  Tooltip heart
+   $('[data-toggle="tooltip"]').tooltip();
+
+ });
+
+ $('.owl-carousel').owlCarousel({
+     loop:true,
+     margin:50,
+     responsiveClass:true,
+     responsive:{
+         0:{
+             items:1,
+             nav:true
+         },
+         600:{
+             items:2,
+             nav:true
+         },
+         1000:{
+             items:3,
+             nav:true,
+             loop:true
+         }
+     }
+ });
