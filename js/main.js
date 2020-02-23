032f3594f479207d10ab3376569a4b2c5f15ef60
Rajan Jangra <rajangra@users.noreply.github.com>
2020-02-22 19:21:53 -0500

    about page changes
    minor css changes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
-$(function () {
-  count = 0;
-  wordsArray = ["UX","Product","experience"];
-  setInterval(function () {
-    count++;
-    $("#word").fadeOut(400, function () {
-      $(this).text(wordsArray[count % wordsArray.length]).fadeIn(400);
-    });
-  }, 5000);
-});
+// Word changer
 
-$('#work, #work1').click(function(){
-    $("html, body").animate({ scrollTop: 700 }, 500);
-    return false;
- });
+// $(function () {
+//   count = 0;
+//   wordsArray = ["UX","Product","experience"];
+//   setInterval(function () {
+//     count++;
+//     $("#word").fadeOut(400, function () {
+//       $(this).text(wordsArray[count % wordsArray.length]).fadeIn(400);
+//     });
+//   }, 5000);
+// });
+
+
 
  $(document).ready(function(){
   //  Tooltip heart
@@ -28,25 +27,46 @@ $('#work, #work1').click(function(){
         }
     });
 
- });
 
- $('.owl-carousel').owlCarousel({
-     loop:true,
-     margin:50,
-     responsiveClass:true,
-     responsive:{
-         0:{
-             items:1,
-             nav:true
-         },
-         600:{
-             items:2,
-             nav:true
-         },
-         1000:{
-             items:3,
-             nav:true,
-             loop:true
-         }
-     }
- });
+    $('#work, #work1').click(function(){
+        $("html, body").animate({ scrollTop: 700 }, 500);
+        return false;
+     });
+
+     //
+    //  $('.owl-carousel').owlCarousel({
+    //      loop:true,
+    //      margin:50,
+    //      responsiveClass:true,
+    //      responsive:{
+    //          0:{
+    //              items:1,
+    //              nav:true
+    //          },
+    //          600:{
+    //              items:2,
+    //              nav:true
+    //          },
+    //          1000:{
+    //              items:3,
+    //              nav:true,
+    //              loop:true
+    //          }
+    //      }
+    //  });
+
+
+    /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+      } else {
+        document.getElementById("navbar").style.top = "-75px";
+      }
+      prevScrollpos = currentScrollPos;
+    }
+
+
+});
