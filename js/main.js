9de05ec96d34cc554e3232dbb245aba5e15bac2c
Rajan Jangra <32543470+rajangra@users.noreply.github.com>
2020-11-18 19:30:33 -0500

    major update to single page
    new portfolio case studies are WIP. temporary single landing page with new layout

--- js/main.js
+++ js/main.js
@@ -13,60 +13,58 @@
 
 
 
- $(document).ready(function(){
+$(document).ready(function(){
   //  Tooltip heart
-   $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="tooltip"]').tooltip();
 
 
-    $(document).click(function (event) {
-        var clickover = $(event.target);
-        var $navbar = $(".navbar-collapse");
-        var _opened = $navbar.hasClass("in");
-        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
-            $navbar.collapse('toggle');
-        }
-    });
-
-
-    $('#work, #work1').click(function(){
-        $("html, body").animate({ scrollTop: 700 }, 500);
-        return false;
-     });
-
-     //
-    //  $('.owl-carousel').owlCarousel({
-    //      loop:true,
-    //      margin:50,
-    //      responsiveClass:true,
-    //      responsive:{
-    //          0:{
-    //              items:1,
-    //              nav:true
-    //          },
-    //          600:{
-    //              items:2,
-    //              nav:true
-    //          },
-    //          1000:{
-    //              items:3,
-    //              nav:true,
-    //              loop:true
-    //          }
-    //      }
-    //  });
-
-
-    /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-    var prevScrollpos = window.pageYOffset;
-    window.onscroll = function() {
-      var currentScrollPos = window.pageYOffset;
-      if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-      } else {
-        document.getElementById("navbar").style.top = "-75px";
-      }
-      prevScrollpos = currentScrollPos;
+  $(document).click(function (event) {
+    var clickover = $(event.target);
+    var $navbar = $(".navbar-collapse");
+    var _opened = $navbar.hasClass("in");
+    if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+      $navbar.collapse('toggle');
     }
+  });
+
+
+  $('#work, #work1').click(function(){
+    $("html, body").animate({ scrollTop: 600 }, 500);
+    return false;
+  });
+
+  //
+  //  $('.owl-carousel').owlCarousel({
+  //      loop:true,
+  //      margin:50,
+  //      responsiveClass:true,
+  //      responsive:{
+  //          0:{
+  //              items:1,
+  //              nav:true
+  //          },
+  //          600:{
+  //              items:2,
+  //              nav:true
+  //          },
+  //          1000:{
+  //              items:3,
+  //              nav:true,
+  //              loop:true
+  //          }
+  //      }
+  //  });
+
+
+  /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+  // $(document).scroll(function() {
+  //   var y = $(this).scrollTop();
+  //   if (y < 150) {
+  //     $('#navbar').fadeIn();
+  //   } else {
+  //     $('#navbar').fadeOut();
+  //   }
+  // });
 
 
 });
