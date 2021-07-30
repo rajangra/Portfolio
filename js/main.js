838ad3db06178f1048c2b0d1f18a8d9f6d212257
canadataxinc <rajanonrs@gmail.com>
2021-07-30 13:30:31 -0400

    Major redesign. July 30th, 2021
    - new styling
    - updated content

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
-})
+});
+
+let hour = new Date().getHours();
+const newHour = ((hour<12 && "morning" || hour<18 && "afternoon" || "evening"));
+
+document.getElementById("timeOfDay").innerHTML = newHour;
