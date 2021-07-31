51b1161e9676279028a3e734019e6bbcf402b0ac
canadataxinc <rajanonrs@gmail.com>
2021-07-30 22:55:40 -0400

    accordion chevron toggle

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
+// trigger tooltip
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
 
+
+// set time of day on homepage
 let hour = new Date().getHours();
 const newHour = ((hour<12 && "morning" || hour<18 && "afternoon" || "evening"));
 
 document.getElementById("timeOfDay").innerHTML = newHour;
+
+
+// toggle accordion chevron
+$('.btn-accordion').click(function() {
+  $(".icon-chevron").toggleClass('la-angle-up');
+  $(".icon-chevron").toggleClass('la-angle-down');
+});
