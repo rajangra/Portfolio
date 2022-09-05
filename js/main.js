98ede69c7a57af0c6b39e0c7f0155f546c376ffd
Rajan <rajanonrs@gmail.com>
2022-09-05 09:00:48 -0700

    added 3 updated case

--- js/main.js
+++ js/main.js
@@ -1,20 +1,37 @@
-// trigger tooltip
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+Fancybox.bind('[data-fancybox="gallery"]', {
+  animated: true,
+  showClass: false,
+  hideClass: false,
+
+  dragToClose: true,
+
+  Image: {
+    zoom: false,
+  },
+
+  Toolbar: {
+    display: [{ id: "counter", position: "center" }, "close"],
+  },
+
+  // Custom animations
+  showClass: "fancybox-zoomIn",
+  hideClass: "fancybox-zoomOut",
 });
 
+// trigger tooltip
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
 
 // set time of day on homepage
 let hour = new Date().getHours();
-const newHour = ((hour<12 && "morning" || hour<18 && "afternoon" || "evening"));
+const newHour =
+  (hour < 12 && "morning") || (hour < 18 && "afternoon") || "evening";
 
 document.getElementById("timeOfDay").innerHTML = newHour;
 
-
 // toggle accordion chevron
-$('.btn-accordion').click(function() {
-  $(".icon-chevron").toggleClass('la-angle-up');
-  $(".icon-chevron").toggleClass('la-angle-down');
+$(".btn-accordion").click(function () {
+  $(".icon-chevron").toggleClass("la-angle-up");
+  $(".icon-chevron").toggleClass("la-angle-down");
 });
-
- 
