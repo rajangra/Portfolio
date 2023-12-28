fec62f62f2dc198f86e73ae73fe91661d0b03909
RJ <rajanixd@gmail.com>
2023-12-27 20:26:54 -0800

    updated index, added typeit js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,39 @@
-Fancybox.bind('[data-fancybox="gallery"]', {
-  animated: true,
-  showClass: false,
-  hideClass: false,
+$(document).ready(function () {
+  Fancybox.bind('[data-fancybox="gallery"]', {
+    animated: true,
+    showClass: false,
+    hideClass: false,
 
-  dragToClose: true,
+    dragToClose: true,
 
-  Image: {
-    zoom: false,
-  },
+    Image: {
+      zoom: false,
+    },
 
-  Toolbar: {
-    display: [{ id: "counter", position: "center" }, "close"],
-  },
+    Toolbar: {
+      display: [{ id: "counter", position: "center" }, "close"],
+    },
 
-  // Custom animations
-  showClass: "fancybox-zoomIn",
-  hideClass: "fancybox-zoomOut",
-});
+    // Custom animations
+    showClass: "fancybox-zoomIn",
+    hideClass: "fancybox-zoomOut",
+  });
 
-// trigger tooltip
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-});
+  // trigger tooltip
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
 
-// set time of day on homepage
-let hour = new Date().getHours();
-const newHour =
-  (hour < 12 && "morning") || (hour < 18 && "afternoon") || "evening";
+  // set time of day on homepage
+  let hour = new Date().getHours();
+  const newHour =
+    (hour < 12 && "morning") || (hour < 18 && "afternoon") || "evening";
 
-document.getElementById("timeOfDay").innerHTML = newHour;
+  document.getElementById("timeOfDay").innerHTML = newHour;
 
-// toggle accordion chevron
-$(".btn-accordion").click(function () {
-  $(".icon-chevron").toggleClass("la-angle-up");
-  $(".icon-chevron").toggleClass("la-angle-down");
+  // toggle accordion chevron
+  $(".btn-accordion").click(function () {
+    $(".icon-chevron").toggleClass("la-angle-up");
+    $(".icon-chevron").toggleClass("la-angle-down");
+  });
 });
